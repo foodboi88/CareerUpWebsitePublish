de640581b7f1c46d7944a98098f7c2e9ca066634
Trung Hieu Do <hieu.yl.ptit1312@gmail.com>
2022-11-03 02:14:22 +0700

    little fix

--- src/components/CCareerDetailModel.tsx
+++ src/components/CCareerDetailModel.tsx
@@ -3,6 +3,7 @@ import type { ColumnsType } from 'antd/es/table';
 import { Modal, Space, Table } from 'antd';
 import React from 'react';
 import detailCareerImg from '../images/detal_career_tmp.png'
+import Temporary_Career from '../images/Temporary_Career.png'
 import { Specialized } from '../common/define-type';
 
 interface MyProps {
@@ -30,7 +31,7 @@ const CCareerDetailModel = ({ isShow, setIsShowModal,clickedSpecialized }: MyPro
                     <div className='career-advisor-modal-content'>
                         <div style={{ textAlign: 'center', fontSize: 20, fontWeight: 600 }}><h5>{clickedSpecialized.specialized_name}</h5></div>
                         <p style={{ color: '#646A6C' }}>{clickedSpecialized.specialized_description}</p>
-                        <img style={{ width: '100%' }} src={detailCareerImg} />
+                        <img style={{ width: '100%' }} src={Temporary_Career} />
                     </div>
                 </div>
 
