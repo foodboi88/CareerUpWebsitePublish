9dab5dc161ab475d267b2a1aa0956e2610b36947
Trung Hieu Do <hieu.yl.ptit1312@gmail.com>
2022-11-05 10:38:59 +0700

    Update CHeader.tsx

--- src/components/CHeader.tsx
+++ src/components/CHeader.tsx
@@ -162,7 +162,7 @@ const CHeader = (props: Myprops) => {
                   trigger={["click"]}
 
               >
-                  <div className='mr-2 mt-4' style={{cursor:'pointer'}} onClick={(e) => e.preventDefault()}><UnorderedListOutlined /></div>
+                  <div className='mr-2 mt-4' style={{cursor:'pointer'}} onClick={(e) => e.preventDefault()}><UnorderedListOutlined style={{fontSize:'25px'}} /></div>
               </Dropdown>
             </div>
             
